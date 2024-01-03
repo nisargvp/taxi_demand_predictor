c1e1446016e8d081e9cc866cd9df4b10e166fce7
Nisarg Patel <nisargvp@umich.edu>
2024-01-02 20:09:56 -0800

    model_train_pipeline nb complete & model registered to hopsworks

--- src/paths.py
+++ src/paths.py
@@ -6,6 +6,8 @@ DATA_DIR = PARENT_DIR / 'data'
 RAW_DATA_DIR = PARENT_DIR / 'data' / 'raw'
 TRANSFORMED_DATA_DIR = PARENT_DIR / 'data' / 'transformed'
 
+MODELS_DIR = PARENT_DIR / 'models'
+
 if not Path(DATA_DIR).exists():
     os.mkdir(DATA_DIR)
 
@@ -14,3 +16,6 @@ if not Path(RAW_DATA_DIR).exists():
     
 if not Path(TRANSFORMED_DATA_DIR).exists():
     os.mkdir(TRANSFORMED_DATA_DIR)
+    
+if not Path(MODELS_DIR).exists():
+    os.mkdir(MODELS_DIR)
